4a6f2a5273991f683b75fd64ecdf533deb1da53e
roko bobic <roko.bobic@gmail.com>
2020-10-20 14:59:16 +0200

    basic edge detection

---
+++ go.mod
@@ -0,0 +1,5 @@
+module grid2excel
+
+go 1.15
+
+require gocv.io/x/gocv v0.24.0
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"math"
+
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	//load document
+	//save pages as images?
+	//open cv- detect edges
+	//ocr - get bonding boxes
+	// map edges to row/columns and letters
+	detectEdges("test1.jpg")
+}
+
+func detectEdges(filename string) {
+	mat := gocv.IMRead(filename, gocv.IMReadColor)
+
+	matCanny := gocv.NewMat()
+	matGray := gocv.NewMat()
+
+	matLines := gocv.NewMat()
+
+	window := gocv.NewWindow("detected lines")
+
+	gocv.CvtColor(mat, &matGray, gocv.ColorBGRToGray)
+
+	gocv.Canny(matGray, &matCanny, 100, 150)
+	gocv.HoughLinesPWithParams(matCanny, &matLines, 1, math.Pi/180, 200, 2, 4)
+
+	fmt.Println(matLines.Cols())
+	fmt.Println(matLines.Rows())
+	for i := 0; i < matLines.Rows(); i++ {
+		pt1 := image.Pt(int(matLines.GetVeciAt(i, 0)[0]), int(matLines.GetVeciAt(i, 0)[1]))
+		pt2 := image.Pt(int(matLines.GetVeciAt(i, 0)[2]), int(matLines.GetVeciAt(i, 0)[3]))
+		gocv.Line(&mat, pt1, pt2, color.RGBA{0, 255, 0, 50}, 2)
+	}
+
+	for {
+		window.ResizeWindow(1500, 2000)
+		window.IMShow(mat)
+		if window.WaitKey(10) >= 0 {
+			break
+		}
+	}
+}
